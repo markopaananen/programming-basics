3763fd31d4e0ed18e0875e88d8a3080e8c7dfedb
markopaananen <marko.paananen@student.saimia.fi>
2017-09-12 14:34:39 +0300

    Task3  done

--- condititonal-statements/Task1/Program.cs
+++ condititonal-statements/Task1/Program.cs
@@ -21,18 +21,18 @@ namespace Task1
             // if > 0
             if (evaluteNumber > 0)
             {
-                Console.WriteLine("Number [0] is positive", evaluteNumber);
+                Console.WriteLine("Number {0} is positive", evaluteNumber);
             }
             // if < 0
             if (evaluteNumber < 0)
             {
-                Console.WriteLine("Number [0] is negative", evaluteNumber);
+                Console.WriteLine("Number {0} is negative", evaluteNumber);
             }
 
             // if == 0
             else
             {
-                Console.WriteLine("Number [0] equals 0", evaluteNumber);
+                Console.WriteLine("Number {0} equals 0", evaluteNumber);
             }
 
             // wait
