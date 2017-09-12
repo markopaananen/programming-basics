4a8fed3f6e5c982a274508da97435b1668cff43d
markopaananen <marko.paananen@student.saimia.fi>
2017-09-12 13:17:42 +0300

    Test2

--- condititonal-statements/Task1/Program.cs
+++ condititonal-statements/Task1/Program.cs
@@ -6,7 +6,38 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // get value
+            Console.WriteLine("Please enter one number!");
+
+            // read uservalue variable
+            string uservalue;
+            uservalue = Console.ReadLine();
+
+            // Evaluate value
+            int evaluteNumber;
+            evaluteNumber = int.Parse(uservalue);
+
+            // compare value
+            // if > 0
+            if (evaluteNumber > 0)
+            {
+                Console.WriteLine("Number [0] greater than 0", evaluteNumber);
+            }
+            // if < 0
+            if (evaluteNumber < 0)
+            {
+                Console.WriteLine("Number [0] less than 0", evaluteNumber);
+            }
+
+            // if == 0
+            else
+            {
+                Console.WriteLine("Number [0] equal than 0", evaluteNumber);
+            }
+
+            // wait
+            Console.ReadKey();
+
         }
     }
 }
