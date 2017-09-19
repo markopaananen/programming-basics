4ae33e4f5278500648afb741db13cfef34e4dd8b
markopaananen <marko.paananen@student.saimia.fi>
2017-09-19 12:48:22 +0300

    looptask2 done

--- loop-construction/task1/Program.cs
+++ loop-construction/task1/Program.cs
@@ -9,20 +9,31 @@ namespace task1
             Console.WriteLine("Hello World! \n");
             double fractorNumber;
             string userInput;
+            
 
             Console.WriteLine("Input fractorialnumeber ?");
             userInput = Console.ReadLine();
             fractorNumber = double.Parse(userInput);
             
-            double fractorResult = 1;
-            for (int i = 0;i < fractorNumber; i++)
+            if (fractorNumber >= 0)
             {
-                fractorResult = fractorResult + (fractorResult * i);                
-            }
+                double fractorResult = 1;
+                for (int i = 0;i < fractorNumber; i++)
+                {
+                    fractorResult = fractorResult + (fractorResult * i);                
+                }
 
-            Console.WriteLine("Number {0} Fractor is: {1} ", fractorNumber, fractorResult);
-            Console.Write("\n Press any key to continue...");
-            Console.ReadKey();
+                Console.WriteLine("Number {0} Fractor is: {1} ", fractorNumber, fractorResult);
+                Console.Write("\n Press any key to continue...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Indefinable fractornumber!");
+                Console.Write("\n Press any key to continue...");
+                Console.ReadKey();
+
+            }
         }
     }
 }
---
+++ loop-construction/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \n");
+            string userInput;
+            double countNumber;
+            Console.WriteLine("Input how many numbers to couont together?");
+            userInput = Console.ReadLine();
+            countNumber = double.Parse(userInput);
+
+            if (countNumber >= 1)
+            {
+                double result = 0;
+                for (int i = 0; i <= countNumber; i++)
+                {
+                    //result = result + (result + i);
+                    result = result + i;
+                }
+
+                Console.WriteLine("Sum of {0} first numbers is {1} ", countNumber, result);
+                Console.Write("\n Press any key to continue...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Usersinput value {0} must be > 1:", userInput);
+                Console.Write("\n Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
