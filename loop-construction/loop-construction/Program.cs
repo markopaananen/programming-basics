415bdb62c13ed8736ada7b8f81fd5491d1201778
markopaananen <marko.paananen@student.saimia.fi>
2017-09-19 12:02:39 +0300

    Loop task1 done

---
+++ loop-construction/loop-construction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loop_construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ loop-construction/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \n");
+            double fractorNumber;
+            string userInput;
+
+            Console.WriteLine("Input fractorialnumeber ?");
+            userInput = Console.ReadLine();
+            fractorNumber = double.Parse(userInput);
+            
+            double fractorResult = 1;
+            for (int i = 0;i < fractorNumber; i++)
+            {
+                fractorResult = fractorResult + (fractorResult * i);                
+            }
+
+            Console.WriteLine("Number {0} Fractor is: {1} ", fractorNumber, fractorResult);
+            Console.Write("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
