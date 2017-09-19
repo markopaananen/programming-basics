536d26cd074bd84aa9b7518cc45e2a6ea26497cc
markopaananen <marko.paananen@student.saimia.fi>
2017-09-19 14:31:47 +0300

    looptask5 done

---
+++ loop-construction/task5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Sum given first even and odd numbers together\n");
+            string userInput;
+            double countNumber;
+            Console.WriteLine("Input how many numbers to couont together?");
+            userInput = Console.ReadLine();
+            countNumber = Math.Abs(double.Parse(userInput));
+
+            double evenResult = 0;
+            double oddResult = 0;
+            for (int i = 0; i <= countNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenResult = evenResult + i;
+                }
+                else
+                {
+                    oddResult = oddResult + i;
+                }
+            }
+            double result = evenResult + oddResult;
+
+            string evenLine = "Sum of {0} first numbers is {1} and sum of evens {2} and odds {3} ";
+            string oddLine = "Sum of -{0} first numbers is -{1} and sum of evens -{2} and odds -{3} ";
+            string writeLine = evenLine;
+            if (int.Parse(userInput) < 0) writeLine = oddLine;
+            
+            Console.WriteLine(writeLine, countNumber, result, evenResult, oddResult);
+            Console.Write("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
