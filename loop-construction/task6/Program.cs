d84d56190df96c9987f06608a35aa9b16f378baf
markopaananen <marko.paananen@student.saimia.fi>
2017-09-20 09:29:28 +0300

    looptask6 done

--- loop-construction/task6/Program.cs
+++ loop-construction/task6/Program.cs
@@ -6,7 +6,13 @@ namespace task6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!, ten first ineger and squareroots");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Integer : {0}, Squareroot : {1}", i, Math.Sqrt(i));
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
