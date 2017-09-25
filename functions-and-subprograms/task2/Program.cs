04dfc41ab297c8d5c4329e160562b702284aeb6e
markopaananen <marko.paananen@student.saimia.fi>
2017-09-25 12:02:59 +0300

    functiontask3 done

---
+++ functions-and-subprograms/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user for input
+            Console.Write("Get Random number (use dot to separate desimals): ");
+            string userInput = Console.ReadLine();
+            double.TryParse(userInput, out double inputnumber1);
+            Console.Write("Get another random number: ");
+            userInput = Console.ReadLine();
+            double.TryParse(userInput, out double inputnumber2);
+            Console.WriteLine("Given number {0} was lesser.\n", MinNumber(inputnumber1, inputnumber2));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            double MinNumber(double number1, double number2)
+            {
+                // Return the lesser number
+                return Math.Min(number1, number2);
+            }
+        }
+    }
+}
---
+++ functions-and-subprograms/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double userDefinedNumber = 0;
+            double inputMinNumber = 4;
+            double inputMaxNumber = 10;
+
+            userDefinedNumber = AskForANumber(inputMinNumber, inputMaxNumber);
+            Console.WriteLine("\nUser gives a number {0}", userDefinedNumber);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            double AskForANumber(double minNumber, double maxNumber)
+            {
+                double number = 0;
+                Console.Write("Give a number between {0} and {1}: ", minNumber, maxNumber);
+                string userInput = Console.ReadLine();
+                while ((!double.TryParse(userInput, out number)) || (number < minNumber) || (number > maxNumber))
+                {
+                    // Prompt user for input
+                    Console.Clear();
+                    Console.WriteLine("Given number {0} is outside range {1} and {2}. New try. ", userInput, minNumber, maxNumber);
+                    Console.Write("Give a number {0} and {1}: ", minNumber, maxNumber);
+                    userInput = Console.ReadLine();
+                }
+                return number;
+            }
+        }
+    }
+}
