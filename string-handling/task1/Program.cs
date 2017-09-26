08cee5743f6eccf319adda75a5571aa3db3ea43b
markopaananen <marko.paananen@student.saimia.fi>
2017-09-26 09:28:54 +0300

    stringhandling task1 done.

---
+++ string-handling/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, number of string character");
+            Console.WriteLine("----------------------------------------------------\n");
+            Console.WriteLine("Please let some random sentense here. \n");
+            string userInput = Console.ReadLine();
+            int inputLength = userInput.Length;
+            Console.WriteLine("\n String lengght is {0} character. ", inputLength);
+            Console.WriteLine("\n Press any key to continue", inputLength);
+            Console.ReadKey();
+        }
+    }
+}
+
