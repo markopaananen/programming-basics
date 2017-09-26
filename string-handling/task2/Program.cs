9a3b17fbc2c57b867bbcdfe37cb4b195bef92009
markopaananen <marko.paananen@student.saimia.fi>
2017-09-26 11:13:14 +0300

    stringhandling task2 done.

---
+++ string-handling/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, replace character!");
+            Console.WriteLine("----------------------------------------------------\n");
+            Console.WriteLine("Please let some random sentense here. \n");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Let character to replace. \n");
+            string replaceChar = Console.ReadLine();
+            Console.WriteLine("Let compensatory character. \n");
+            string compensatoryChar = Console.ReadLine();
+            string newSting = userInput.Replace(replaceChar,compensatoryChar);
+            Console.WriteLine(newSting);
+            Console.ReadKey();
+
+            int inputLenght = newSting.Length;
+            for(int i =1; i< inputLenght; i++)
+            {
+                Console.WriteLine(newSting.Substring(i, 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ string-handling/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s = "aaaaabbbcccccccdd";
+        Char charRange = 'c';
+        int startIndex = s.IndexOf(charRange);
+        int endIndex = s.LastIndexOf(charRange);
+        int length = endIndex - startIndex + 1;
+        Console.WriteLine("{0}.Substring({1}, {2}) = {3}",
+                          s, startIndex, length,
+                          s.Substring(startIndex, length));
+        Console.ReadKey();
+    }
+}
+
